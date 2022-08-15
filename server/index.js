ef267b4704e0bddab40f30051b5e3172b8d37b30
Телицын Никита <dmtvn.labs@yandex.ru>
2022-08-15 18:10:21 +0300

    commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,26 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const events = require('events')
+const e = require("express");
+const PORT = process.env.PORT
+const emitter = new events.EventEmitter()
 
+app.use(require('cors')())
 
-app.listen(3000, () => {
+app.get('get-messages', (req, res) => {
+    emitter.once('newMessage', (message) => {
+        res.json(message)
+    })
+})
+
+app.post('new-messages', (req, res) => {
+    const message = req.body
+    emitter.emit('newMessage', message)
+
+    res.status(200)
+})
+
+app.listen(PORT, () => {
     console.log('Server started')
 })
